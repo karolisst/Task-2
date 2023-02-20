d6a04f045816f38cde524c9095714762894775c8
karolis steponavicius <karolis.stpn@gmail.com>
2023-02-20 13:42:30 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+
+const button = document.getElementById("btn__element");
+const output = document.getElementById("btn__state");
+let count = 0;
+
+button.onclick = function () {
+  count++;
+  output.innerHTML = count;
+};
